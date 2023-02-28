795fda5def967cc874c8de217b2505c5e5e0266b
Nuria Diaz <ndiaz@robotnik.es>
2023-02-28 13:53:02 +0100

    Python code that creates the logrotate file for the logs of a certain directory.

---
+++ script/logrotate.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+import os
+
+dir_logs = 'logs'
+
+contenido = os.listdir(dir_logs)
+
+directorios = []
+for i in contenido:
+    directorios.append(str(i))
+
+
+logrotate = open("logrotate.d/logrotate.conf","w")
+
+logrotate.write("# Include all log files in ~/workspaces/rb_theron/logs/\n")
+logrotate.write("# The files will be rotated up to 4 times when they exceed the size 1k or a week has passed.\n") 
+logrotate.write("# As the file is rotated every 7 days, 4 times maximum, there will be no files older than 28 days.\n")
+logrotate.write("# The copytruncate allows a copy of the file to be rotated. \n")
+logrotate.write("\n")
+
+for i in directorios:
+    logrotate.write("~/workspaces/rb_theron/logs/" + i +"/*.log\n")
+
+
+logrotate.write("{\n")
+logrotate.write("maxsize 1k\n")
+logrotate.write("weekly\n")
+logrotate.write("rotate 4\n")
+logrotate.write("nocompress\n")
+logrotate.write("notifempty\n")
+logrotate.write("create\n")
+logrotate.write("}\n")
+
+
